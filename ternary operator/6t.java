3ebee0b7a52ff06d544c83b55deb231f540414d9
botshatarun-create <botshatarun@gmail.com>
2025-10-03 22:49:57 +0530

    Create 6t.java

---
+++ ternary operator/6t.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        
+        int bill=650;
+        boolean ispremium=false;
+        if((bill>=500)||(ispremium==true)){
+             System.out.println("Free Delivery");
+        }
+        else{
+             System.out.println("Delivery Charges Apply");
+        }
+       
+    }
+}
