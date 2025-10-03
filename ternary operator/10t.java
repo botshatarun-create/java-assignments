6773a31809e376e7c058d4964d23f8a67ff1062b
botshatarun-create <botshatarun@gmail.com>
2025-10-03 23:10:22 +0530

    Create 10t.java

---
+++ ternary operator/10t.java
@@ -0,0 +1,11 @@
+class SumFirst20 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 1; i <= 20; i++) {
+            sum += i;   // using only += operator
+        }
+
+        System.out.println("Sum of first 20 natural numbers = " + sum);
+    }
+}
