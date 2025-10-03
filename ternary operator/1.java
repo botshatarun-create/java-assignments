8ce0b2d831899e7ac31eda6fd6e6c310797b10ba
botshatarun-create <botshatarun@gmail.com>
2025-10-03 22:18:13 +0530

    Create 1.java

---
+++ ternary operator/1.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        double r=5;
+        double pi =3.14159;
+        double area=pi*r*r;
+        double circumference=2*pi*r;
+        System.out.println("area of circle:"+area);
+        System.out.println("circumference of circle :"+circumference);
+        
+    }
+}
