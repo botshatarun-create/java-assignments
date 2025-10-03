3846ccdccf41c145a31a29e1efd3adf2d01d6a8a
botshatarun-create <botshatarun@gmail.com>
2025-10-03 22:38:38 +0530

    Create 2t.java

---
+++ ternary operator/2t.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args) {
+      int a=20;
+      int b=6;
+      int c=0;
+      for(;a >=b;){
+          a=a-b;
+         
+        c++;
+                       }
+       
+       System.out.println("Quotient = " + c);
+       System.out.println("Remainder = " + a);
+
+    }
+}
