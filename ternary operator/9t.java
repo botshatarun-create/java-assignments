7eb6e42d17a11782053d5b7f3f0f271c9f7fd6a6
botshatarun-create <botshatarun@gmail.com>
2025-10-03 23:01:29 +0530

    Create 9t.java

---
+++ ternary operator/9t.java
@@ -0,0 +1,11 @@
+class AssignmentOps {
+    public static void main(String[] args) {
+        int num = 8;
+
+        num *= 3;   // num = num * 3 → 24
+        num += 12;  // num = num + 12 → 36
+        num %= 7;   // num = num % 7 → 1
+
+        System.out.println("Final value of num = " + num);
+    }
+}
