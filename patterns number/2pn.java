97cd3fda5e0eb618b0a7f5dab3f4d2560b95e5ae
botshatarun-create <botshatarun@gmail.com>
2025-10-12 22:45:54 +0530

    Update 2pn.java

--- patterns number/2pn.java
+++ patterns number/2pn.java
@@ -1,9 +1,9 @@
-class Main {
+ class Main {
     public static void main(String[] args) {
         int a = 0, b = 1;
         int row = 1;
         
-        do {
+        while(row <= 4) {
             for(int col = 1; col <= row; col++) {
                 System.out.print(a + " ");
                 int next = a + b;
@@ -12,6 +12,6 @@ class Main {
             }
             System.out.println();
             row++;
-        } while(row <= 4);
+        }
     }
 }
